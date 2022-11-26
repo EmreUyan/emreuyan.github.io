2184c20827f97fd846b603edaef176c63926efc6
EMRE UYAN <tayfuremreuyan@gmail.com>
2022-11-26 23:57:48 +0300

    new commits

---
+++ script/script.js
@@ -0,0 +1,9 @@
+var sites = [
+    '../index.html',
+    '../sayfa/fpschess.html'
+  ];
+  
+  function randomSite() {
+    var i = parseInt(Math.random() * sites.length);
+    location.href = sites[i];
+  }
